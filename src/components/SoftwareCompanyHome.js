8dc1fe0a94d9fa85e28b3fc498e54a038fbaf125
Bossortega666 <jortega@ip-depot.com.mx>
2025-06-12 22:11:52 -0600

    ENDingassassadddddddddddsasdd

--- src/components/SoftwareCompanyHome.js
+++ src/components/SoftwareCompanyHome.js
@@ -179,7 +179,7 @@ export default function SoftwareCompanyHome() {
                     transition={{ duration: 0.8 }}
                     className="text-5xl font-extrabold mb-4 drop-shadow-lg"
                   >
-                    IP DEPOT CAMBIO END 
+                    IP DEPOT CAMBIO END dsasa
                   </motion.h1>
                   <p className="text-xl text-gray-100 max-w-xl drop-shadow-sm">
                     Innovación y Desarrollo de Software a la Medida
